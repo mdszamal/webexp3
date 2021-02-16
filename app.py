eb7fb5195000dd5ed7db4d891b808fa3359e8dcc
mdszamal <78942241+mdszamal@users.noreply.github.com>
2021-02-16 11:17:57 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 09:26:10 2021
+
+@author: mshahzamal
+"""
+
+from flask import Flask, request, jsonify, render_template
+
+#naming our app as app
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
